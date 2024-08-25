490fc550305f5cfdc1a315f89196a5b681c40535
Nikhil Raj <131578072+NikhilRajOfficial@users.noreply.github.com>
2024-08-25 14:01:44 +0530

    Add files via upload

---
+++ src/main/java/com/nikhil/PaymentIntegration/PaymentApplicationDetails/PaymentDetails.java
@@ -0,0 +1,47 @@
+package com.nikhil.PaymentIntegration.PaymentApplicationDetails;
+
+import java.util.Map;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties("paytm.payment.sandbox")
+public class PaymentDetails {
+
+    private String merchantId;
+	
+	private String merchantKey;
+	
+	private String channelId;
+	
+	private String website;
+	
+	private String industryTypeId;
+	
+	private String paytmUrl;
+	
+	private Map<String, String> details;
+
+    public PaymentDetails() {}
+
+    
+    
+    public PaymentDetails(String merchantId, String merchantKey, String channelId, String website,
+			String industryTypeId, String paytmUrl, Map<String, String> details) {
+		super();
+		this.merchantId = merchantId;
+		this.merchantKey = merchantKey;
+		this.channelId = channelId;
+		this.website = website;
+		this.industryTypeId = industryTypeId;
+		this.paytmUrl = paytmUrl;
+		this.details = details;
+	}
+
+}
